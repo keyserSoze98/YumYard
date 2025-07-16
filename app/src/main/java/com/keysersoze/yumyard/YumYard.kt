465e30804e30a561b40d7c94800921422c9bc5b0
Kishan <kishan.das.0298@gmail.com>
2025-07-16 11:09:39 +0530

    Ad restricted to under-18

--- app/src/main/java/com/keysersoze/yumyard/YumYard.kt
+++ app/src/main/java/com/keysersoze/yumyard/YumYard.kt
@@ -1,6 +1,8 @@
 package com.keysersoze.yumyard
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.keysersoze.yumyard.fcm.FcmTokenManager
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -9,4 +11,13 @@ import javax.inject.Inject
 class YumYard : Application() {
     @Inject
     lateinit var fcmTokenManager: FcmTokenManager
+
+    override fun onCreate() {
+        super.onCreate()
+        val requestConfiguration = RequestConfiguration.Builder()
+            .setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_PG)
+            .build()
+        MobileAds.setRequestConfiguration(requestConfiguration)
+        MobileAds.initialize(this)
+    }
 }
